b1fb0f30155631ad921666eeb36368b129ed26b9
bf_tsai <tsai19911026@gmail.com>
2024-01-13 23:53:04 +0800

    fix: 🐛 index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import LoginView from '../views/LoginView.vue';
 import ProductList from '../views/ProductListView.vue';
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
         {
           path: '/LoginView',
